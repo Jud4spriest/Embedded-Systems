0764e1e00b8e879b2055e93c8d21043baa8dfeaa
Marcos Azevedo <mazevedo.snts@gmail.com>
2021-07-25 00:48:18 -0300

    25-07-2021-00:47

--- Threads/Threads.ino
+++ Threads/Threads.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Thread.h"
+#include "ThreadController.h"
 
+void piscaLed1(){
+  
+}
+
+void piscaLed2(){
+  
+}
+
+ThreadController cpu;
+
+Thread t1;
+Thread t2;
+
+void setup() {
+  t1.setInterval(500);
+  t1.onRun(piscaLed1);
+  
+  t1.setInterval(300);
+  t1.onRun(piscaLed2);
+
+  cpu.add(&t1);
+  cpu.add(&t2);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+  cpu.run();
+  
 }
